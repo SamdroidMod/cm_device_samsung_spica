dccb7c818d5f9c4dbf3f5b55313da0c9d77b4c5c
LeshaK <leshak@leshak.ru>
2010-09-27 22:55:46 +0400

    init samsung/spica device for CyanogenMod

---
+++ BoardConfig.mk
@@ -0,0 +1,93 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/samsung/spica/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RECOVERY := true
+TARGET_NO_KERNEL := true
+
+TARGET_BOARD_PLATFORM := s3c6410
+TARGET_ARCH_VARIANT := armv5te-vfp
+
+TARGET_CPU_ABI := armeabi
+
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := true
+BUILD_WITH_ALSA_UTILS := true
+
+BOARD_HAVE_BLUETOOTH := true
+BT_USE_BTL_IF := true
+BT_ALT_STACK := true
+
+BRCM_BTL_INCLUDE_A2DP := true
+BRCM_BT_USE_BTL_IF := true
+
+BOARD_EGL_CFG := device/samsung/spica/prebuilt/egl/egl.cfg
+
+BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
+BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
+BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
+BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
+# The size of a block that can be marked bad.
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_USES_FFORMAT := true
+BOARD_RECOVERY_IGNORE_BOOTABLES := true
+
+BOARD_BOOT_DEVICE := /dev/block/bml7
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p2
+BOARD_DATA_FILESYSTEM := rfs
+BOARD_DATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_HAS_DATADATA := true
+BOARD_DATADATA_DEVICE := /dev/block/stl10
+BOARD_DATADATA_FILESYSTEM := rfs
+BOARD_DATADATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_SYSTEM_DEVICE := /dev/block/stl9
+BOARD_SYSTEM_FILESYSTEM := rfs
+BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
+BOARD_CACHE_DEVICE := /dev/block/stl11
+BOARD_CACHE_FILESYSTEM := rfs
+BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+# The following is the *actual* sd card, but those don't mount in recovery.
+# The mmc blocks aren't even available.
+# Only internal storage (mmcblk0) is available.
+#BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1
+#BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1p1
+#BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+#BOARD_USES_BMLUTILS := true
+
+#TARGET_PREBUILT_KERNEL := device/samsung/spica/kernel
+
+BOARD_WLAN_DEVICE           := eth0
+WIFI_DRIVER_MODULE_PATH     := "/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/rtecdc.bin nvram_path=/system/etc/nvram.txt"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+
+BOARD_HAVE_GPS_HARDWARE := true
+BOARD_GPS_LIBRARIES := libsecgps libsecril-client
---
+++ full_spica.mk
@@ -0,0 +1,131 @@
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160 \
+    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libargs=-d /dev/ttyS0 \
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=15 \
+    ro.product.multi_touch_enable=true \
+    ro.product.max_num_touch=2
+
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+
+#
+# Copy Spica specific prebuilt files
+#
+
+#
+# Wifi
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/wifi/libwlmlogger.so:system/lib/libwlmlogger.so \
+    device/samsung/spica/prebuilt/spica/wifi/libwlservice.so:system/lib/libwlservice.so \
+    device/samsung/spica/prebuilt/spica/wifi/nvram.txt:system/etc/nvram.txt \
+    device/samsung/spica/prebuilt/spica/wifi/rtecdc.bin:system/etc/rtecdc.bin \
+    device/samsung/spica/prebuilt/spica/wifi/nvram_mfg.txt:system/etc/nvram_mfg.txt \
+    device/samsung/spica/prebuilt/spica/wifi/rtecdc_mfg.bin:system/etc/rtecdc_mfg.bin \
+    device/samsung/spica/prebuilt/spica/wifi/bcm_supp.conf:system/etc/bcm_supp.conf \
+    device/samsung/spica/prebuilt/spica/wifi/wifi.conf:system/etc/wifi.conf \
+    device/samsung/spica/prebuilt/spica/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/spica/prebuilt/spica/wifi/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/samsung/spica/prebuilt/spica/wifi/wlservice:system/bin/wlservice \
+    device/samsung/spica/prebuilt/spica/wifi/wpa_supplicant:system/bin/wpa_supplicant
+
+#
+# Display (2D)
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/gralloc-libs/libs3c2drender.so:system/lib/libs3c2drender.so \
+    device/samsung/spica/prebuilt/spica/gralloc-libs/libsavscmn.so:system/lib/libsavscmn.so \
+    device/samsung/spica/prebuilt/spica/gralloc-libs/hw/gralloc.GT-I5700.so:system/lib/hw/gralloc.GT-I5700.so
+
+#
+# Display (3D)
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/fimg-libs/egl.cfg:system/lib/egl/egl.cfg \
+    device/samsung/spica/prebuilt/spica/fimg-libs/libChunkAlloc.so:system/lib/egl/libChunkAlloc.so \
+    device/samsung/spica/prebuilt/spica/fimg-libs/libEGL_fimg.so:system/lib/egl/libEGL_fimg.so \
+    device/samsung/spica/prebuilt/spica/fimg-libs/libGLESv1_CM_fimg.so:system/lib/egl/libGLESv1_CM_fimg.so \
+    device/samsung/spica/prebuilt/spica/fimg-libs/libGLESv2_fimg.so:system/lib/egl/libGLESv2_fimg.so
+
+#
+# Keys
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/keys/s3c-keypad-rev0020.kl:system/usr/keylayout/s3c-keypad-rev0020.kl \
+    device/samsung/spica/prebuilt/spica/keys/sec_headset.kl:system/usr/keylayout/sec_headset.kl \
+    device/samsung/spica/prebuilt/spica/keys/s3c-keypad-rev0020.kcm.bin:system/usr/keychars/s3c-keypad-rev0020.kcm.bin
+
+#
+# Sensors, Lights etc
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/hw/copybit.GT-I5700.so:system/lib/hw/copybit.GT-I5700.so \
+    device/samsung/spica/prebuilt/spica/hw/lights.GT-I5700.so:system/lib/hw/lights.GT-I5700.so
+
+#    device/samsung/spica/prebuilt/spica/hw/sensors.GT-I5700.so:system/lib/hw/sensors.GT-I5700.so 
+
+#
+# Vold
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/vold/vold.fstab:system/etc/vold.fstab
+
+#
+# RIL
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/ril/drexe:system/bin/drexe \
+    device/samsung/spica/prebuilt/spica/ril/efsd:system/bin/efsd \
+    device/samsung/spica/prebuilt/spica/ril/rilclient-test:system/bin/rilclient-test \
+    device/samsung/spica/prebuilt/spica/ril/libsec-ril.so:system/lib/libsec-ril.so
+
+#
+# Audio
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/spica/prebuilt/spica/audio/asound.conf:system/etc/asound.conf
+
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/samsung/spica/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# we have enough storage space to hold precise GC data
+#PRODUCT_TAGS += dalvik.gc.type-precise
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.startheapsize=8m \
+    dalvik.vm.heapsize=48m
+
+# Vibrant uses high-density artwork where available
+PRODUCT_LOCALES := mdpi
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_NAME := full_spica
+PRODUCT_DEVICE := spica
+PRODUCT_MODEL := GT-I5700
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
